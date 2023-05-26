dba8d211c9f00a731c9fee18c0476607e243051b
Shukrullojon <47257193+Shukrullojon@users.noreply.github.com>
2023-05-26 10:14:35 +0500

    Some changes

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,11 +17,21 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::create([
+        /*User::create([
             "name" => "Shukrullo",
             "email" => "shukrullobk@gmail.com",
             "token" => Str::random(80),
             "password" => Hash::make('1272256a'),
+            "theme" => "default",
+            "created_at" => date("Y-m-d H:i:s"),
+            "updated_at" => date("Y-m-d H:i:s"),
+        ]);*/
+
+        User::create([
+            "name" => "GPWORK",
+            "email" => "gpworkucoin@gmail.com",
+            "token" => Str::random(80),
+            "password" => Hash::make('GPWORK_Whf9r'),
             "theme" => "default",
             "created_at" => date("Y-m-d H:i:s"),
             "updated_at" => date("Y-m-d H:i:s"),
