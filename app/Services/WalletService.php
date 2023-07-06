88d3a7532ed2066999c2fb467fc62bd9a2f6ff32
Shukrullojon <shukrullobk@gmail.com>
2023-07-06 16:44:33 +0500

    debit ucoin

--- app/Services/WalletService.php
+++ app/Services/WalletService.php
@@ -11,7 +11,7 @@ class WalletService
     public static function debit($data)
     {
         $card = Card::where('token', $data['token'])->first();
-        if ($card->balance > $data['amount']) {
+        if ($card->balance >= $data['amount']) {
             $card->update([
                 'balance' => $card->balance - $data['amount'],
             ]);
